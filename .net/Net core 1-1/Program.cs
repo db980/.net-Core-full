337836ace5eabc4b895e502a42835217c7b20e7f
db980 <51694342+db980@users.noreply.github.com>
2022-09-14 14:41:30 +0530

    testing
    Testing

---
+++ .net/Net core 1-1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Net_core_1_1
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello World!");
+    //    }
+    //}
+
+    public class Program
+    {
+        //NonStatic method
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(@"The Sum of {0} and {1}, is {2} ", x, y, (x + y));
+        }
+        //Static Method
+        public static string Greetings(string name)
+        {
+            return "Hello @" + name;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            //calling non static method through object
+            obj.Add(100, 100);
+            //Calling static method with class name
+            string GreetingsMessage = Program.Greetings("Pranaya");
+            Console.WriteLine(GreetingsMessage);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
